418aedfcfb420f5adb04c487601c76c6f6834ec9
Nathan <nathmclean@gmail.com>
2018-01-27 14:16:17 -0800

    add shitty implementation of player projectiles

---
+++ Assets/Scripts/PlayerProjectileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectileController : MonoBehaviour {
+
+    public GameObject playerObject;
+    private Collider2D playerCollider;
+
+	void Start()
+    {
+        playerCollider = playerObject.GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject p = GameObject.Find("Player");
+        Collider2D pc = p.GetComponent<Collider2D>();
+        if (collision.collider != pc && collision.otherCollider != playerCollider)
+            gameObject.SetActive(false);
+    }
+
+}
