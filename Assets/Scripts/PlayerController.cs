cdc1c8f4aaba74887d4d0b82f4ff5a742899df4b
Nathan <nathmclean@gmail.com>
2018-01-27 00:42:28 -0800

    update player movement/actions
    - add a boost action
    - remove "win" text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,48 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 //Adding this allows us to access members of the UI namespace including Text.
 using UnityEngine.UI;
 
+internal delegate void PlayerActionEffect();
+
+internal class PlayerAction
+{
+    public UnityEngine.KeyCode keycode;
+    public float cooldown;
+    public float cooldownRemaining = 0.0f;
+    public PlayerActionEffect effect;
+
+    public PlayerAction(UnityEngine.KeyCode keycode, float cooldown, PlayerActionEffect effect)
+    {
+        this.keycode = keycode;
+        this.cooldown = cooldown;
+        this.effect = effect;
+    }
+}
+
 public class PlayerController : MonoBehaviour {
 
-	public float speed;				//Floating point variable to store the player's movement speed.
-	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
-	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
+    // Tunable parameters
+	public float speed;
+    public float boostCooldown;
+    public float boostSpeedMultiplier;
+    public float boostDuration;
 
+    private List<PlayerAction> moveset = new List<PlayerAction>();
+    private float speedMultiplier = 1.0f;
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
-	private int count;				//Integer to store the number of pickups collected so far.
+
 
 	// Use this for initialization
 	void Start()
 	{
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
-
-		//Initialize count to zero.
-		count = 0;
-
-		//Initialze winText to a blank string since we haven't won yet at beginning.
-		winText.text = "";
-
-		//Call our SetCountText function which will update the text with the current value for count.
-		SetCountText ();
+        moveset.Add(new PlayerAction(KeyCode.LeftShift, boostCooldown, Boost));
 	}
+
+    void Boost()
+    {
+        speedMultiplier = boostSpeedMultiplier;
+    }
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
 	void FixedUpdate()
 	{
-		//Store the current horizontal input in the float moveHorizontal.
+        foreach (var move in moveset)
+        {
+            if (Input.GetKey(move.keycode) && move.cooldownRemaining <= 0)
+            {
+                move.effect();
+                move.cooldownRemaining = move.cooldown;
+            }
+            else if (move.cooldownRemaining > 0)
+            {
+                move.cooldownRemaining -= Time.deltaTime;
+            }
+        }
+
+        // make boost decay linearly over time
+        if (speedMultiplier > 1.0f)
+            speedMultiplier -= (boostSpeedMultiplier - 1.0f) * (Time.deltaTime / boostDuration);
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-
-		//Store the current vertical input in the float moveVertical.
 		float moveVertical = Input.GetAxis ("Vertical");
-
-		//Use the two store floats to create a new Vector2 variable movement.
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-		rb2d.velocity = movement * speed;
+		rb2d.velocity = movement * speed * speedMultiplier;
 	}
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
@@ -53,24 +82,6 @@ public class PlayerController : MonoBehaviour {
 		{
 			//... then set the other object we just collided with to inactive.
 			other.gameObject.SetActive(false);
-			
-			//Add one to the current value of our count variable.
-			count = count + 1;
-			
-			//Update the currently displayed count by calling the SetCountText function.
-			SetCountText ();
 		}
-	}
-
-	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
-	void SetCountText()
-	{
-		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
-		countText.text = "Count: " + count.ToString ();
-
-		//Check if we've collected all 12 pickups. If we have...
-		if (count >= 12)
-			//... then set the text property of our winText object to "You win!"
-			winText.text = "You win!";
 	}
 }
