517343760155e9679fef1d9a776f3e8f1a26f7fa
weihanderz <wei.protein@gmail.com>
2018-01-28 01:15:37 -0800

    Added Title Screen

---
+++ Assets/Scripts/ColorTweener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorTweener : MonoBehaviour
+{
+    public Color startColor;
+    public Color targetTarget;
+    public bool loop;
+    public float changeTime = 1.0f;
+    public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+    private string loopType = "none";
+
+    void Start()
+    {
+        if (loop)
+        {
+            loopType = "pingpong";
+        }
+
+        iTween.ValueTo(gameObject, iTween.Hash("from", startColor, "to", targetTarget, "time", changeTime, "easetype", "easeInCubic", "onUpdate", "UpdateColor", "loopType", loopType));
+    }
+    void UpdateColor(Color newColor)
+    {
+        Image image = GetComponent<Image>();
+        image.color = newColor;
+    }
+}
