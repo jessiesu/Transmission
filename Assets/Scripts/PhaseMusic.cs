7cf5ab6d2fb76087e554db0a01d67f7f6ecf4bd1
Jessie Su <jess0411@gmail.com>
2018-01-28 14:27:23 -0800

    Added background audio tracks

---
+++ Assets/Scripts/PhaseMusic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PhaseMusic : MonoBehaviour
+{
+    public AudioClip redAudio;
+    public AudioClip blueAudio;
+    private AudioSource audioSource;
+
+    public float currentTime = 0;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRedTrack()
+    {
+        PlayTrack(redAudio);
+    }
+
+    public void PlayBlueTrack()
+    {
+        PlayTrack(blueAudio);
+    }
+
+    public void PlayTrack(AudioClip track)
+    {
+        if (audioSource.isPlaying)
+        {
+            currentTime = audioSource.time;
+        }
+
+        audioSource.clip = track;
+        audioSource.time = currentTime;
+        audioSource.Play();
+    }
+}
